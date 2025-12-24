c72ef68b0a879628bfb4c7e973a5468229a9c70b
Chris Vinod <chrisvinod12@gmail.com>
2025-12-24 14:03:37 +0530

    Refactor rendering in index.js to remove StrictMode wrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Updated way
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
